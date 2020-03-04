f5d63d1c8b07e0a9dbb231ff636d41993b2133c1
nuDoc <61743947+nuDoc@users.noreply.github.com>
2020-03-04 12:03:21 +0100

    Add files via upload

--- core.js
+++ core.js
@@ -1,29 +1,19 @@
-"use strict";
+var swiper = new Swiper ('.swiper-container', {
 
+    slidesPerView: 1,
+    direction: 'vertical',
 
-//Sequential For Loop
-//Processing of Array
-function loop(data) {
-	var output = "";
-	for(var i = 0; i < data.length; i++) {
-		output += " <a href=\"" + data[i][0] + "\">" + data[i][1] + "</a>";
-	}
-	document.body.innerHTML = "<span>" + output + "</span>";   
-}
-/////////////////////
-//PageChange Function
-function turnpage(event) {
-	if (event.target.tagName == "BODY" || event.target.tagName == "DIV") {
-		(event.clientY < event.target.offsetHeight/2) ? state-- : state++;
-		state = Math.min(4, Math.max(0, state));
-		loop(contents[state]);
-	}
-}
+    mousewheel: {
+    	invert: false,
+  	},
 
+    pagination: {
+    	el: '.swiper-pagination',
+    	type: 'bullets'
+    },
+    paginationClickable: true,
 
-var state = 0;
-loop(contents[state]);
-document.body.addEventListener("click", turnpage);
-var div = document.createElement("div");
-document.body.appendChild(div);
-div.innerHTML = "&#8595;";
+    spaceBetween: 30,
+    loop:true,
+
+});
